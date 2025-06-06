761b0ca3c7a18aaf63aa3f00230c3adb913f7286
theharshchavan <harshchavan6362@gmail.com>
2025-06-06 13:28:04 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [ code, setCode ] = useState(`// Write your JavaScript code here
+
+  console.log('Hello, world!');`)
+
+  const [ review, setReview ] = useState(``)
+
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
+
+  async function reviewCode() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    setReview(response.data)
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review">Review</div>
+        </div>
+        <div className="right">
+          {review ? <Markdown
+
+            rehypePlugins={[ rehypeHighlight ]}
+
+          >{review}</Markdown> : <div className="placeholder">Click Review to get AI feedback</div>}
+        </div>
+      </main>
+    </>
+  )
+}
+
+
+
+export default App
