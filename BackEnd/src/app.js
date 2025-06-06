5b1e52db6ff4db37fbb9938187ff037876fa2087
theharshchavan <harshchavan6362@gmail.com>
2025-06-06 15:02:13 +0530

    issue solve

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -7,13 +7,21 @@ dotenv.config()
 const app = express()
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST'],
+  origin: function (origin, callback) {
+    // Allow undefined for tools like Postman
+    if (!origin || origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
   credentials: true,
 };
 
+// MUST come before routes
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options("*", cors(corsOptions)); 
 
 
 app.use(express.json())
